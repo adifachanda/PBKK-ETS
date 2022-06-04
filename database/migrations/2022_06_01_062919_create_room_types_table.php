f610e2a49c48a91b6f7b4b31c3464012f4a2d887
Alfin Indrawan <alfinindrawan54@gmail.com>
2022-06-04 10:09:22 +0700

    insert data for room_types

--- database/migrations/2022_06_01_062919_create_room_types_table.php
+++ database/migrations/2022_06_01_062919_create_room_types_table.php
@@ -16,8 +16,15 @@ return new class extends Migration
         Schema::create('room_types', function (Blueprint $table) {
             $table->id();
             $table->string('room_name',500);
-            $table->unsignedDecimal('price', 10, 2);
+            $table->unsignedInteger('price');
         });
+
+        DB::table('room_types')->insert([
+            ['room_name' => 'Single Bed', 'price' => 250000],
+            ['room_name' => 'Double Bed', 'price' => 500000],
+            ['room_name' => 'VIP Queen', 'price' => 1500000],
+            ['room_name' => 'VIP King', 'price' => 2000000],
+        ]);
     }
 
     /**
